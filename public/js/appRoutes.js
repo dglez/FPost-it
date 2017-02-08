a980f14b6e880ee2e8b225865f240b53f511d3de
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-02-08 01:12:48 -0500

    Added all the routes

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,3 +1,18 @@
 /**
  * Created by Daniel Gonzalez {DG} on 2/3/17.
  */
+
+var appRoutes = angular.module('appRoutes', []);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        //home page
+        .when('/', {
+            templateURL: 'views/main.html',
+            controller: 'MainController'
+        });
+
+    $locationProvider.html5Mode(true);
+}]);
